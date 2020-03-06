6a3b8bf7bf33dc5bbd81953d85b4f84eb929cef7
Amit <amitmoyal95@walla.com>
2020-03-06 09:30:52 -0500

    Main now reads the text file

--- myProject.c
+++ myProject.c
@@ -2,10 +2,13 @@
 #include <stdio.h>
 #include <string.h>
 
+#define LINE_LENGTH 80
 
 int main(int argc, char **argv){
 	FILE *fd, *fdob, *fdext, *fdent;
-
+	char buffer[LINE_LENGTH] = {0};
+	int i,ch,line=1;
+	
 	/*No arguments error*/
 	if(argc == 1){
 		printf("No files entered\n");
@@ -18,5 +21,23 @@ int main(int argc, char **argv){
 		exit(0);
 	}
 	
+	/*Reading the text line by line*/
+	while(!feof(fd)){
+		i = 0;
+
+		while((ch = fgetc(fd))!=EOF && ch!='\n'){
+
+			if(i<LINE_LENGTH){
+				buffer[i++] = ch;
+			}else{
+				printf("\nLine %d is too long\n",line);
+				break;
+			}
+		}
+
+		buffer[i]='\0';
+		printf("%s\n",buffer);
+		line++;
+	}
 	return 0;
 }
